53602e5dfd383ed7d68261e98a8bead0e627b93f
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-02-20 14:20:23 +0100

    add test with in memory rbac

---
+++ src/memory.test.ts
@@ -0,0 +1,116 @@
+import { describe, test, expect, beforeEach } from "@jest/globals";
+
+import { createInMemoryRBAC } from "./memory";
+import { type RBAC, createRBAC } from "./rbac";
+
+const permissions = [
+  "service.component.create",
+  "service.component.read",
+  "service.component.update",
+  "service.component.delete",
+  "service.component.list",
+] as const;
+
+type Permission = (typeof permissions)[number];
+
+const roles = [
+  "roles/service.componentCreator",
+  "roles/service.componentReader",
+  "roles/service.componentUpdater",
+  "roles/service.componentDeleter",
+  "roles/service.componentAdmin",
+] as const;
+
+type Role = (typeof roles)[number];
+
+let rbac: RBAC<Permission, Role>;
+
+beforeEach(() => {
+  rbac = createRBAC(createInMemoryRBAC());
+});
+
+describe("permissions to roles", () => {
+  test("assigns a permission to a role", async () => {
+    const permission: Permission = "service.component.create";
+    const role: Role = "roles/service.componentCreator";
+
+    await rbac.assignPermissionToRole(role, permission);
+
+    const hasPermission = await rbac.roleHasPermission(role, permission);
+    expect(hasPermission).toBe(true);
+  });
+
+  test("removes a permission from a role", async () => {
+    const permission: Permission = "service.component.create";
+    const role: Role = "roles/service.componentCreator";
+
+    await rbac.assignPermissionToRole(role, permission);
+    await rbac.removePermissionFromRole(role, permission);
+
+    const hasPermission = await rbac.roleHasPermission(role, permission);
+    expect(hasPermission).toBe(false);
+  });
+});
+
+describe("subjects to roles", () => {
+  test("assigns a role to a subject", async () => {
+    const role: Role = "roles/service.componentCreator";
+    const subject = "user:example@example.com";
+
+    await rbac.assignRoleToSubject(subject, role);
+
+    const hasRole = await rbac.subjectHasRole(subject, role);
+    expect(hasRole).toBe(true);
+  });
+
+  test("removes a role from a subject", async () => {
+    const role: Role = "roles/service.componentCreator";
+    const subject = "user:example@example.com";
+
+    await rbac.assignRoleToSubject(subject, role);
+    await rbac.removeRoleFromSubject(subject, role);
+
+    const hasRole = await rbac.subjectHasRole(subject, role);
+    expect(hasRole).toBe(false);
+  });
+});
+
+describe("subjects to permissions", () => {
+  test("checks if a subject has a permission", async () => {
+    const permission: Permission = "service.component.create";
+    const role = "roles/service.componentAdmin";
+    const subject = "user:example@example.com";
+
+    await rbac.assignPermissionToRole(role, permission);
+    await rbac.assignRoleToSubject(subject, role);
+
+    const hasPermission = await rbac.subjectHasPermission(subject, permission);
+    expect(hasPermission).toBe(true);
+  });
+
+  test("removes role after being assigned to a user", async () => {
+    const permission: Permission = "service.component.create";
+    const role = "roles/service.componentAdmin";
+    const subject = "user:example@example.com";
+
+    await rbac.assignPermissionToRole(role, permission);
+    await rbac.assignRoleToSubject(subject, role);
+    await rbac.removeRoleFromSubject(subject, role);
+
+    const hasPermission = await rbac.subjectHasPermission(subject, permission);
+    expect(hasPermission).toBe(false);
+  });
+
+  test("removes permission after being assigned to a role", async () => {
+    const permission: Permission = "service.component.create";
+    const role = "roles/service.componentAdmin";
+    const subject = "user:example@example.com";
+
+    await rbac.assignPermissionToRole(role, permission);
+    await rbac.assignRoleToSubject(subject, role);
+    await rbac.removePermissionFromRole(role, permission);
+
+    const hasPermission = await rbac.subjectHasPermission(subject, permission);
+    expect(hasPermission).toBe(false);
+  });
+});
---
+++ src/memory.ts
@@ -0,0 +1,56 @@
+import type { RBAC } from "./rbac";
+
+export function createInMemoryRBAC<
+  P extends string = string,
+  R extends string = string
+>(): RBAC<P, R> {
+  const rolePermissions = new Map<R, Set<P>>();
+  const subjectRoles = new Map<string, Set<R>>();
+
+  return {
+    async assignPermissionToRole(role: R, permission: P): Promise<void> {
+      if (!rolePermissions.has(role)) {
+        rolePermissions.set(role, new Set<P>());
+      }
+
+      rolePermissions.get(role)?.add(permission);
+    },
+
+    async removePermissionFromRole(role: R, permission: P): Promise<void> {
+      rolePermissions.get(role)?.delete(permission);
+    },
+
+    async roleHasPermission(role: R, permission: P): Promise<boolean> {
+      return rolePermissions.get(role)?.has(permission) ?? false;
+    },
+
+    async assignRoleToSubject(subject: string, role: R): Promise<void> {
+      if (!subjectRoles.has(subject)) {
+        subjectRoles.set(subject, new Set<R>());
+      }
+
+      subjectRoles.get(subject)?.add(role);
+    },
+
+    async removeRoleFromSubject(subject: string, role: R): Promise<void> {
+      subjectRoles.get(subject)?.delete(role);
+    },
+
+    async subjectHasRole(subject: string, role: R): Promise<boolean> {
+      return subjectRoles.get(subject)?.has(role) ?? false;
+    },
+
+    async subjectHasPermission(
+      subject: string,
+      permission: P
+    ): Promise<boolean> {
+      for (const role of subjectRoles.get(subject) ?? new Set<R>()) {
+        if (rolePermissions.get(role)?.has(permission)) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+  };
+}
